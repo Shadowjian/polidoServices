e04137e6164b6ffb27f450f3ef4a3544d861d4a7
Shadowjian <karakalel.adorable@gmail.com>
2022-11-26 12:33:53 +0800

    lasjdf

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const style = {
+  color: 'red',
+  fontSize: '3rem'
+}
+
 
 
 function Navbar() {
   return (
     <>
-    <Link to="/" style={{color: 'red'}}>Home</Link>
+    <Link to="/" style={style}>Home</Link>
     <Link to="/about">About</Link>
     <Link to="/contact">Search</Link>
     </>
