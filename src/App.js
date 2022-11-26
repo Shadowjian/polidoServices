e034660a56b16782598b5ba932f05bfe61ba3fe4
Shadowjian <karakalel.adorable@gmail.com>
2022-11-26 12:59:37 +0800

    without pushin

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
       <Route  path='/about' element={<About />}/>
       <Route  path='/search' element={<Search />}/>
     </Routes>
-    <Home />
     </BrowserRouter>
     </>
   );
