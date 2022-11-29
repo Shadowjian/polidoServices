61789cb69ff71fee945a96136ff562ad9bffb5d8
poldong <poldong2019@gmail.com>
2022-11-29 10:12:09 +0800

    add

--- src/routes/about/About.js
+++ src/routes/about/About.js
@@ -1,11 +1,89 @@
 import React from 'react'
 import './About.css'
+ 
 
 
 
 function About() {
   return (
-    <div>About TEAM</div>
+    <> 
+      <div id='aboutBody'>
+
+        <div id='contentContainer'>
+        
+          <h1 id='about'>About Polido Services</h1>
+
+          <h2 id='headOne'>Who We Are</h2>
+          <p id='p-One'>
+                    Polido services are the first services marketplace in the Philippines. We are driving a shift towards a demand-based, digital economy for service businesses.
+
+              We are passionate about helping people to empower them to live their work by building their businesses from the ground up and becoming financially and professionally independent.
+
+              We're a team of energetic, passionate, and dedicated individuals. As the First growing services marketplace, we have vast ambitions as we continue to expand across the country.
+          </p>
+
+          <h2 id='headTwo'>Our Goal in the community </h2>
+          <p id='p-Two'>
+              We are passionate about what we do, our team is proud to support thousands of Filipinos to get started off the ground.
+
+              One of our goals is to connect clients to our community of professional individualsÂ available to hire by the hour or project; provide flexibility to work when it suits you, outside the archaic 9-to-5 day; and enable people to live their work dream.
+          </p>
+
+          <h2 id='headThree'>A POSSIONATE TEAM </h2>
+          <p id='p-Three'>
+              Polido services assembled a team of the best and brightest minds in the business.
+
+              We're all dedicated to the global ambitions of our business, whilst maintaining a collaborative, passionate, and family-like approach with shared responsibility and passion for everyone using our service.
+          </p>
+
+          <h2 id='headFour'>THE FOUNDERS</h2>
+          <p id='p-Four'>
+            Polido services are Founded by successful serial entrepreneurs <strong> Hajji, Mark Leo, and Joseph Gee</strong> with a vision and passion for helping Individual professionals get any job done easier and more efficiently.
+          </p>
+
+        </div>
+
+        <div id='divCardsContainer'>
+
+          <li id='cardOne'>
+          
+            CARDS PHOTO HAJJI
+          </li>
+          
+          <li id='cardTwo'>
+         
+            <li id='yellowBar'>
+
+           <label id='nameTag'> CARDS PHOTO JOSEPH</label>
+               {/* blue bar */}
+            </li>
+            
+            <li id='devImage'>
+               
+            </li>
+
+            <div id='iconContainer'>
+
+              <li id='progIcons'> 
+              
+               
+
+              </li>
+              
+            </div>
+           
+          </li>
+
+          <li id='cardThree'>CARDS PHOTO POLDZ</li>
+          
+        </div>
+
+       
+
+      
+        
+      </div>
+    </>
   )
 }
 
