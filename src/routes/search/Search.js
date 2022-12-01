2549b982ff3cafd5e5c7b353eec16c8d339347e5
Shadowjian <karakalel.adorable@gmail.com>
2022-12-01 20:22:38 +0800

    added new stuff

--- src/routes/search/Search.js
+++ src/routes/search/Search.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Search.css'
 import RightSection from './components/RightSection'
 import LeftSection from './components/LeftSection'
 import SearchSection from './components/SearchSection'
-
+import Sellers from './components/Sellers'
 
 // PAGE LAYOUT
 
 function Search() {
-  const [state, setState] = useState('test')
+  const [sellers, setSellers] = useState([])
+  const [searchInput, setSearchInput] = useState('')
 
-  const handleInput =(e)=> {
-      setState(e.target.value)
-      }
-      
+  useEffect(()=>{
+    setSellers(Sellers)
+  },[])
+
+  console.log(searchInput)
+
+  const searchSellers =(e)=> {
+      setSearchInput(e.target.value)
+      setSellers(Sellers.filter((seller, i)=>{
+        return seller.skills.includes(searchInput.toLowerCase())
+      }))
+  }
+  console.log(sellers)
   return (
     <div className='page-container'>
       <div className='left'>
-        <SearchSection fn={handleInput}/>
-        <LeftSection result={state}/>
+        <SearchSection searchFn={searchSellers} searchInput={searchInput}/>
+        <LeftSection result={searchInput} sellers={sellers}/>
       </div>
       <RightSection />
     </div>
