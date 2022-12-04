ef30a8f49b6dfd63d970d3b8f4be1e97ebe7b1b8
Shadowjian <karakalel.adorable@gmail.com>
2022-12-04 18:22:02 +0800

    updating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,23 +17,53 @@ function NavModal({hideNav}) {
   )
 }
 
-
-
-function Login({hideLogin}) {
+function Login({hideLogin, showSignUp}) {
   return (
       <form action="" id='login-modal'>
         <i className="fa-solid fa-x close-btn" onClick={hideLogin}></i>
         <div className="join-modal">
-          <h3 className="join-heading">Join Polido Services</h3>
+          <h3 className="join-heading">Polido Services</h3>
           <div className="btnl btn-fb">Continue with Facebook</div>
           <div  className="btnl btn-google">Continue with Google</div>
           <div className="or"> OR </div>
           <div className="input">
-              <input type="email" name="email" id="email" placeholder="Enter your email" />
+              <input type="email" name="email" id="email" placeholder="Please enter your email" />
+          </div>
+          <div className="input">
+              <input type="password" name="email" id="password" placeholder="Please enter your password" />
+          </div>
+          <div>
+            Forgot Password?
           </div>
           <div  className="btnl login-continue">Continue</div>
           <div className="login-member">
-            Already a member? <span className="ismember">Sign in</span>
+            New to Pollido Services? <span className="ismember" onClick={showSignUp}>Sign Up</span>
+          </div>
+      </div>
+    </form>
+  )
+}
+
+
+function SignUp({hideSignUp}) {
+  return (
+      <form action="" id='login-modal'>
+        <i className="fa-solid fa-x close-btn" onClick={hideSignUp}></i>
+        <div className="join-modal">
+          <h3 className="join-heading">Join Polido Services</h3>
+          <div className="btnl btn-fb">Sign Up with Facebook</div>
+          <div  className="btnl btn-google">Sign Up with Google</div>
+          <div className="or"> OR </div>
+          <div className="input">
+              <input type="email" name="email" id="email" placeholder="Please enter your email" />
+          </div>
+          <div className="input">
+              <input type="password" name="email" id="password" placeholder="Minimum 6 characters with a number and a letter" />
+          </div>
+          
+          <div  className="btnl login-continue">Continue</div>
+          <div className="login-member">
+            Already a member? <span className="ismember" onClick={hideSignUp}>Sign in</span>
           </div>
       </div>
     </form>
@@ -45,6 +75,7 @@ function Navbar() {
 const [navigation, setNavigation] = useState(false)
 const [login, setLogin] = useState(false)
 const [loggedIn, setLoggedIn] = useState(false)
+const [signUp, setSignUp] = useState(false)
 
 
 
@@ -62,6 +93,15 @@ const hideNav = () => {
   setLogin(false)
  }
 
+const hideSignUp = () => {
+  setSignUp(false)
+  setLogin(true)
+}
+
+const showSignUp = () => {
+  setSignUp(true)
+  setLogin(false)
+}
 
   return (
     <section id='navbar-wrapper'>
@@ -74,8 +114,7 @@ const hideNav = () => {
         <div className='links'>
           <Link className="link" to="/">Home</Link>
           <Link className="link" to="about">About</Link>
-          {/* <Link className="link" to="search">Search</Link> */}
-          <a href="#footer"className="link">Contacts</a>
+          <Link className="link" to="search">Search</Link>
           
           {loggedIn? <div className="link">Profile</div> :
           <div className="link" onClick={showLogin}>Login</div>}
@@ -101,7 +140,14 @@ const hideNav = () => {
           {login && 
           <>
           <div className='overlay' onClick={hideLogin}></div>
-          <Login hideLogin={hideLogin}/>
+          <Login hideLogin={hideLogin} showSignUp={showSignUp}/>
+          </>}
+        </div>
+      <div>
+          {signUp && 
+          <>
+          <div className='overlay' onClick={hideSignUp}></div>
+          <SignUp hideSignUp={hideSignUp}/>
           </>}
         </div>
 
