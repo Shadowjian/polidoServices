6786f6257139e59df291b1bfc0893cdb9436d427
josephg.crypto <josephg.crypto826@gmail.com>
2022-12-06 02:11:56 +0800

    newupdates1206

---
+++ src/routes/home/components/Faqs.js
@@ -0,0 +1,144 @@
+import React from 'react'
+
+
+function Faqs() {
+    return (
+        <>
+            
+            <div className='faq-container'>
+                
+                <div className='faq-banner'>
+                    <div className='faq-box'>
+                        <div className='faq-border'>
+                            <h1 className='faq-title'>FREQUENTLY ASKED QUESTIONS</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='faqcontent-container'>
+
+                    <div className='content-box'>
+
+                        <div className='faq1-box'>
+                            <h2 className='faq1'>I found the professional I like, how do I get a quote?</h2>
+                            &nbsp;
+
+                            <p>
+                            Getting a booking enables you to contact professional with regards to the details of your order, including your desired appointment time. 
+                            &nbsp;
+                            This provides valuable information for the seller which, in turn, enables the seller to provide a quote customized to you.
+                            </p>
+                            &nbsp;
+                            <p><i>
+                            Note: Get a Quote requests are only available in certain categories and is only optional for a professional to apply so the option may not appear on a professional's profile.
+                            </i></p>
+                            &nbsp;
+                            <p>How to Get a Quote: </p>
+                            &nbsp;
+                            <p>
+                            STEP1: Find the service of choice by browsing through the Services marketplace. It is also possible to go directly to a Seller's profile page if you know their username.
+                            </p>
+                            &nbsp; 
+                            <p>
+                            STEP2: Once logged in, from the Services marketplace tab on the homepage, Search the services you are looking to hire.
+                            </p>
+                            &nbsp;
+                            <p>
+                            STEP3: Click Book now on the Seller's profile.
+                            </p>
+                            &nbsp;   
+                            <p>
+                            STEP4: Click Book now on the Seller's profile.
+                            </p>
+                            &nbsp;   
+                            <p>
+                            STEP5: Describe the specific service and criteria you are looking for. This will help the professional cater to your specific request..
+                            </p>
+                            &nbsp;
+                            <p>
+                            STEP6: Select the service delivery time expected upon booking time.
+                            </p>
+                            &nbsp;
+                            <p>
+                            STEP7: Enter your budget for the service requested (optional).
+                            </p>
+                            &nbsp;
+                            <p>
+                            STEP8: Click Submit Request. The Seller will receive your request and reply with a quote. You may decline or continue to purchase.
+                            </p>
+                            &nbsp; 
+                            <p>
+                            We recommend communicating with the seller through the message box before the purchase to ensure that all questions, requirements, and concerns are addressed prior to any transaction taking place.
+                            </p>
+                            &nbsp;
+                        </div>
+
+                        <div className='faq1-box'>
+                            <h2 className='faq1'>Not finding the service you’re looking for?</h2>
+                            &nbsp;
+
+                            <p>
+                            Our Customer Support team is here to help. Contact us at support@polidoservices.com                            
+                            </p>
+                            &nbsp;
+
+                        
+                        </div>
+
+                        <div className='faq1-box'>
+                            <h2 className='faq1'>Do I get the professional's phone number or email address?</h2>
+                            &nbsp;
+
+                            <p>
+                            Yes. When you completely booked the appointment you set, you’ll get the customer’s phone number and email. You can also contact them directly through your polido services account.                            
+                            </p>
+                            &nbsp;
+
+                        
+                        </div>
+
+                        <div className='faq1-box'>
+                            <h2 className='faq1'>How do I leave a review?</h2>
+                            &nbsp;
+
+                            <p>
+                            Clients can provide feedback as soon as they approve the service delivery. Once the client provides feedback, the professional will be notified that they can provide feedback in return.
+                            </p>
+                            &nbsp;
+
+                            <p>
+                            Both buyers and sellers have up to 10 days to provide feedback. After this time frame, there will no longer be an option to provide feedback.                            
+                            </p>
+                            &nbsp;
+                        </div>
+
+                        <div className='faq1-box'>
+                            <h2 className='faq1'>How do I leave a review?</h2>
+                            &nbsp;
+
+                            <p>
+                            Clients can provide feedback as soon as they approve the service delivery. Once the client provides feedback, the professional will be notified that they can provide feedback in return.
+                            </p>
+                            &nbsp;
+
+                            <p>
+                            Both buyers and sellers have up to 10 days to provide feedback. After this time frame, there will no longer be an option to provide feedback.                            
+                            </p>
+                            &nbsp;
+                        </div>
+
+                    </div>    
+   
+                </div>
+
+            </div>
+
+                
+
+            
+
+        </>
+    )
+}
+
+export default Faqs
---
+++ src/routes/home/components/Privacy.js
@@ -0,0 +1,226 @@
+import React from 'react'
+
+
+function Privacy() {
+    return (
+        <>
+
+            <div className='privacy-container'>
+                
+                <div className='privacy-banner'>
+                    <div className='privacy-box'>
+                        <div className='privacy-border'>
+                            <h1 className='privacy-title'>PRIVACY POLICY</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='privacycontent-container'>
+
+                    <div className='privacycontent-box'>
+
+                        <div className='privacy1-box'>
+
+                            <h2 className='privacy1'>Website Privacy Policy</h2>
+                            &nbsp;
+
+                            <h3 className='privacy1-num'>
+                            1. What is this document and why should you read it?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            This privacy notice explains how and why polidoservices.com uses personal data about individual users of our website www.polidoservices.com (the “Website”) and those that access our services through APIs and through third-parties, including website users, clients and/or professionals (whether prospective, current or past) (together, “you”).
+                            </p>
+                            &nbsp;
+                            <p>
+                            You should read this notice so that you know what we are doing with your personal data.
+                            </p>
+                            &nbsp;                           
+
+
+                            <h3 className='privacy1-num'>
+                            2. Our data protection responsibilities
+                            </h3>
+                            &nbsp;
+                            <p>
+                            “Personal data” is any information that relates to an identifiable natural person. Your name, address and contact details are examples of your personal data, if they identify you.
+                            </p>
+                            &nbsp;
+                            <p>
+                            The term “process” means any activity relating to personal data, including collection, storage, use and transmission.
+                            </p>
+                            &nbsp;
+                            <p>
+                            Whether you are a buyer, a professional or just a website user (or a combination of those) polidoservices.com is a “controller” of your personal data. This means that we make decisions about how and why we process your personal data and are responsible for making sure it is used in accordance with data protection laws.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            3. What types of personal data do we collect and where do we get it from?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            If you are reviewing this then you will fall into at least one of the following categories of individuals about whom we process personal data:
+                            </p>
+                            &nbsp;
+                            <p>
+                            (a) prospective, current or past customers of polidoservices.com who register an account and/or place a service request (“Clients”);
+                            </p>
+                            &nbsp;
+                            <p>
+                            (b) current or past individuals or businesses that register as a Professional on polidoservices.com to provide a service to Clients, or businesses who we identify as being of interest to our clients and who we may then contact with potential business opportunities relating to our clients (“Professionals”); and/or
+                            </p>
+                            &nbsp;
+                            <p>
+                            (c) individuals that access our Website (“Website Users”).
+                            </p>
+                            &nbsp;
+                            <p>
+                            Accordingly, we will collect and process different types of personal data about you depending on which of these categories are relevant to you. The different types of personal data that we collect and the sources we collect it from are summarised below.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            4. Personal Data Categories
+                            </h3>
+                            &nbsp;
+                            <p>
+                            Depending on how you use our Website and services, we will collect different types of personal data about you. For example, this might include things like your name, contact details, details about your business, our correspondence with you, records of your transactions with us, and how you use our services. We need these details to provide our services to you. We have set out more specific details on the categories of personal data we collect and process on Buyers, Professionals and Website Users here.
+                            </p>
+                            &nbsp;
+                            <p>
+                            We obtain this personal data from a number of different sources. For example, this could be from you, our Website, your business website as well as third party sources such as social media. More details of the sources from which we obtain personal data about Buyers, Professionals and Website Users can be found here.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            5.How we use personal data and why?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            We process your personal data for particular purposes, which are relevant to your relationship and engagement with us. We are required by law to always have a “lawful basis” for processing your personal data.
+                            </p>
+                            &nbsp;
+                            <p>
+                            We use the personal data we collect for a number of different reasons, including to provide our services to you or with you, to communicate with you, to verify your identity, to make sure our website and services are functioning properly and provide the best services for you, to keep accurate records and for legal reasons.
+                            </p>
+                            &nbsp;
+                            <p>
+                            We rely on a number of different lawful grounds or ‘bases’ in order to process your personal data. These include that we have your consent to the processing, we need to process your personal data to enter into or perform a contract with you, we need to process your personal data to comply with our legal obligations, and/or the processing is necessary for our legitimate interests (or the legitimate interests of a third party).
+                            </p>
+                            &nbsp;
+                            <p>
+                            More details on the purposes for which we process personal data on Buyers, Professionals, and Website Users, and the lawful bases we rely on.
+                            </p>
+                            &nbsp;
+                            <p>
+                            We may also convert your personal data into statistical or aggregated form to better protect your privacy, or so that you are not identified or identifiable from it. Anonymised data cannot be linked back to you. We may use it to conduct research and analysis, including to produce statistical research and reports. For example, to help us understand and improve the use of our Website.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            6. Who do we share your personal data with and why?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            We may use certain trusted third party companies and individuals to help us provide, analyse, and improve our services (including but not limited to Buyer/Professional verification, data storage, maintenance services, database management, web analytics, payment processing, and improvement of our services). These third parties will process your personal data on our behalf (as our processor). We will disclose your personal data to these parties so that they can perform those necessary functions.
+                            </p>
+                            &nbsp;
+                            <p>
+                            We may also share your information with third parties, who then use your personal data for their own purposes. For example, to process payments from you or where you choose to access our services through such an application. These organisations will also use your personal data for their own, separate purposes (also as a “controller”) – they will have their own privacy notices which you should read, and they have their own responsibilities to comply with applicable data protection laws.
+                            </p>
+                            &nbsp;
+                            <p>
+                            In certain circumstances, where necessary for the purposes set out here, we will also disclose your personal data to:
+                            </p>
+                            &nbsp;
+                            <p>
+                            (a) consultants and professional advisors including legal advisors, auditors and accountants;
+                            </p>
+                            &nbsp;
+                            <p>
+                            (b) business partners and joint ventures;
+                            </p>
+                            &nbsp;
+                            <p>
+                            (c) a prospective seller or buyer of our business and their advisors;
+                            </p>
+                            &nbsp;
+                            <p>
+                            (d) insurers;
+                            </p>
+                            &nbsp;
+                            <p>
+                            (e) courts, court-appointed persons/entities, receivers and liquidators;
+                            </p>
+                            &nbsp;
+                            <p>
+                            (f) third parties where necessary to comply with a legal obligation, to enforce a contract or to protect the rights, property or safety of our employees, customers or others; and
+                            </p>                            
+                            &nbsp;
+                            <p>
+                            (g) to governmental departments, local government, statutory and regulatory bodies including the the police.
+                            </p>
+                            &nbsp;
+
+                            
+                            <h3 className='privacy1-num'>
+                            7. Where is your personal data transferred to?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            When we share personal data about you, as set out above, this may include a transfer of your personal data to recipients outside the Philippines. If any disclosures of personal data mean that your personal data will be transferred outside the Philippine Economic Area, we will only make that transfer in accordance with our obligations under applicable data protection laws.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            8. How long do we keep your personal data for?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            We will only retain your personal data for a limited period of time and for no longer than is necessary for the purposes for which we are processing it for (including as necessary to comply with our legal or regulatory obligations, resolve disputes, and enforce our agreements).
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            9. What are your rights in relation to your personal data and how can you exercise them?
+                            </h3>
+                            &nbsp;
+                            <p>
+                            You have certain legal rights in relation to any personal data about you which we hold. If you have any concerns or queries about this notice or how we process your personal data please contact us using the details below. Each of these are subject to certain conditions.
+                            </p>
+                            &nbsp;
+
+
+                            <h3 className='privacy1-num'>
+                            10. General
+                            </h3>
+                            &nbsp;
+                            <p>
+                            We may update this notice from time to time to reflect changes to the type of personal data that we process and/or the way in which it is processed. We also encourage you to check this notice on a regular basis.
+                            </p>
+                            &nbsp;
+                            <p>
+                            If you want more information about any of the subjects covered in this privacy notice or if you would like to discuss any issues or concerns with us, you can contact us at <i className='email'>support@polidoservices.com.</i>
+                            </p>
+                            &nbsp;
+
+                        </div>
+
+                    </div>    
+   
+                </div>
+
+            </div>
+
+        </>
+    )
+}
+
+export default Privacy
